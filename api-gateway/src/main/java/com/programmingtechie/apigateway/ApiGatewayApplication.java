e3f435a01bb9ae4f578169a03aed74763f18e78b
riBergamo <rrianebergamo@gmail.com>
2023-06-14 15:52:18 -0300

    implementing Api Gateway

--- api-gateway/src/main/java/com/programmingtechie/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/programmingtechie/apigateway/ApiGatewayApplication.java
@@ -1,13 +1,13 @@
-package com.programmingtechie.discoveryserver;
+package com.programmingtechie.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableEurekaServer
-public class DiscoveryServerApplication {
+@EnableDiscoveryClient
+public class ApiGatewayApplication {
     public static void main(String[] args) {
-        SpringApplication.run(DiscoveryServerApplication.class, args);
+        SpringApplication.run(ApiGatewayApplication.class, args);
     }
 }
--- order-service/src/main/java/com/programmingtechie/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/programmingtechie/orderservice/controller/OrderController.java
@@ -17,7 +17,7 @@ public class OrderController {
     @ResponseStatus(HttpStatus.CREATED)
     public String placeOrder(@RequestBody OrderRequest orderRequest) {
         orderService.placeOrder(orderRequest);
-        return "Order placed Successfully";
+        return "Order Placed Successfully";
     }
 
 }
